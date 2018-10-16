64ee11dd915a5618b53e4017dfc06c7848db0d2e
Kevin Pluck <kevinjpluck@gmail.com>
2018-10-17 00:10:36 +0100

    swaps between north, south and global

--- DataLoader.js
+++ DataLoader.js
@@ -1,31 +1,55 @@
 function DataLoader()
 {
-
+    this.north;
+    this.south;
+    this.global;
 }
 
-DataLoader.GetNsidc = function (startYear, endYear, callBack)
+DataLoader.prototype.GetNsidc = function (hemisphere, startYear, endYear, callBack)
 {   
 
-        var years = [];
-        for (var i = startYear; i <= endYear; i++) {
-            years.push(i);
+    if(this[hemisphere])
+    {
+        DataLoader.GotNsidc(this[hemisphere], callBack);
+        return;
+    }
+
+    if(hemisphere == "global")
+    {
+        this.GetGlobal(callBack);
+        return;
+    }
+
+    var years = [];
+    for (var i = startYear; i <= endYear; i++) {
+        years.push(i);
+    }
+
+    var done = years.length;
+    var sum = 0;
+
+    var seaIceData={};
+
+    if(hemisphere == "south")
+        seaIceData.title = "Antarctic Sea Ice Extent";
+    else
+        seaIceData.title = "Arctic Sea Ice Extent";
+
+    seaIceData.startYear = startYear;
+    seaIceData.endYear = endYear;
+    var that = this;
+    $(years).each(function(index, year) {
+        var number = this;
+        $.getJSON("http://nsidc.org/api/seaiceservice/extent/"+hemisphere+"/filled_averaged_data/" + year + "?index=doy&smoothing_window=5", function(data) {
+        seaIceData["year" + year] = data;
+        done -= 1;
+        if(done == 0) 
+        {
+            this[hemisphere] = seaIceData;
+            DataLoader.GotNsidc(seaIceData, callBack);
         }
-
-        var done = years.length;
-        var sum = 0;
-
-        var seaIceData={};
-        seaIceData.startYear = startYear;
-        seaIceData.endYear = endYear;
-        
-        $(years).each(function(index, year) {
-          var number = this;
-          $.getJSON("http://nsidc.org/api/seaiceservice/extent/north/filled_averaged_data/" + year + "?index=doy&smoothing_window=5", function(data) {
-            seaIceData["year" + year] = data;
-            done -= 1;
-            if(done == 0) DataLoader.GotNsidc(seaIceData, callBack);
-          });
-        });
+        }.bind(that));
+    });
 }
 
 DataLoader.GotNsidc = function(seaIceData, callBack)
@@ -34,6 +58,7 @@ DataLoader.GotNsidc = function(seaIceData, callBack)
     
     nsidcDataTable.addColumn('number', 'Day');
     nsidcDataTable.series = [];
+    nsidcDataTable.title = seaIceData.title;
     for(year = seaIceData.startYear; year <= seaIceData.endYear; year++)
     {
         nsidcDataTable.addColumn('number', year);
@@ -64,12 +89,49 @@ DataLoader.GotNsidc = function(seaIceData, callBack)
 
 DataLoader.MakeColor = function(seaIceData, year)
 {
+    if(year == seaIceData.endYear)
+        return "#ff0000";
+
     var range = seaIceData.endYear - seaIceData.startYear;
     var pos = year - seaIceData.startYear;
 
     var lerp = 1 - pos / range;
 
-    var rgb = 127 + Math.floor(lerp * 128);
+    var rgb = 127 + Math.floor(lerp * 100);
     var hex = rgb.toString(16);
     return "#" + hex + hex + hex;
+}
+
+DataLoader.prototype.GetGlobal = function(callBack)
+{
+    if(!this.north || !this.south)
+        return;
+    
+    if(this.global)
+    {
+        DataLoader.GotNsidc(global, callBack);
+        return;
+    }
+    
+    var global = {title: "Global Sea Ice Extent", startYear:this.north.startYear, endYear:this.north.endYear};
+    
+    for(year = global.startYear; year <= global.endYear; year++)
+    {
+        var northData = this.north["year"+year];
+        var southData = this.south["year"+year];
+        global["year"+year] = {};
+        for(day = 1; day <= 366; day++)
+        {
+            if(northData[day] == -1 || southData[day] == -1)
+            {
+                global["year" + year][day] = null;
+                continue;
+            }
+
+            global["year"+year][day] = northData[day] + southData[day];
+        }
+    }
+
+    this.global = global;
+    DataLoader.GotNsidc(global, callBack);
 }
--- LineChart.js
+++ LineChart.js
@@ -1,25 +1,47 @@
-google.charts.load('current', {'packages':['line']});
+google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.setOnLoadCallback(drawChart);
+var dataLoader = new DataLoader();
+var hemisphere = "north";
+var chart;
+var options = {
+    animation:{
+        duration: 1000,
+        easing: 'out',
+        startup: true
+        },
+    width: 900,
+    height: 900,
+    legend:"none",
+    hAxis:{title: "time"},
+    vAxis:
+    {
+        title:"Extent (Millions of square kilometers)",
+        viewWindow:
+        {
+            min: 0, max: 30
+        }
+    }
+};
 
 function drawChart() {
 
-    DataLoader.GetNsidc(1979, 2018, GotNsidc);
-    
+    dataLoader.GetNsidc(hemisphere, 1979, 2018, GotNsidc);
+    if(hemisphere == "north")
+        hemisphere = "south"
+    else
+    if(hemisphere == "south")
+        hemisphere = "global";
+    else
+    if(hemisphere == "global")
+        hemisphere = "north";
 }
 
 function GotNsidc(data)
 {
-    var options = {
-        chart: {
-            title: 'Sea ice!',
-            subtitle: 'in millions of square km'
-        },
-        colors: data.series,
-        width: 900,
-        height: 500
-    };
-
-    var chart = new google.charts.Line(document.getElementById('linechart_material'));
+    options.colors = data.series;
+    options.title = data.title;
+    if(!chart)
+    chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
     chart.draw(data, google.charts.Line.convertOptions(options));
 }
