e7b5901fb0c82715d410a8abadeefd23954acb36
Kevin Pluck <kevinjpluck@gmail.com>
2020-02-09 23:08:19 +0000

    Dynamically generated list

---
+++ Bookmarks.js
@@ -0,0 +1,19 @@
+var bookmarks = 
+[
+    {
+        "description":"What does 400ppm look like?",
+        "url":"http://pic.twitter.com/nQWLFrnswd"
+    },
+    {
+        "description":"Missing Arctic sea ice data",
+        "url":"http://pic.twitter.com/LkC5N7aURO"
+    },
+    {
+        "description":"Never reply to trolls!",
+        "url":"http://pic.twitter.com/eySzEvjS10"
+    },
+    {
+        "description":"Patrick Moore poked a straw in a turtle's eye",
+        "url":"http://pic.twitter.com/8hpKz3UfVB"
+    }
+]
